1b68196192a28f332c8ee15dd4e729447404f193
Karol Mileszko <karol.miles@gmail.com>
2022-10-31 16:48:20 +0000

    Add adjust bag and remove from bag views, and calc subtotal template filter

---
+++ bag/templatetags/bag_tools.py
@@ -0,0 +1,18 @@
+from django import template
+
+
+# In bag.html add: 
+# {% load bag_tools %}
+# {{ item.product.price | calc_subtotal:item.quantity }}
+
+# More info:
+# Creating custom tags and filters
+# https://docs.djangoproject.com/en/4.1/howto/custom-template-tags/
+
+
+register = template.Library()
+
+
+@register.filter(name='calc_subtotal')
+def calc_subtotal(price, quantity):
+    return price * quantity
