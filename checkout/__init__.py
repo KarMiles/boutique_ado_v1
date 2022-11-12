f30610572ec4b98bd788110cab9acc24dd1b59fd
Karol Mileszko <karol.miles@gmail.com>
2022-11-12 10:56:54 +0000

    Add checkout success logic

--- checkout/__init__.py
+++ checkout/__init__.py
@@ -1,1 +1,2 @@
+# This tells Django the name of default Config class for this app:
 default_app_config = 'checkout.apps.CheckoutConfig'
--- checkout/views.py
+++ checkout/views.py
@@ -14,30 +14,6 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    bag = request.session.get('bag', {})
-    if not bag:
-        messages.error(request, "There's nothing in your bag at the moment")
-        return redirect(reverse('products'))
-
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
-
-    # print(intent)
-
-    order_form = OrderForm()  # instance of the order form
-
-    if not stripe_public_key:
-        messages.warning(
-            request,
-            'Stripe public key is missing. \
-                Did you forget to set it in your environment?')
-
     if request.method == 'POST':
         bag = request.session.get('bag', {})
 
@@ -54,7 +30,7 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order_form.save()
+            order = order_form.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
@@ -76,21 +52,17 @@ def checkout(request):
                             order_line_item.save()
                 except Product.DoesNotExist:
                     messages.error(request, (
-                        "One of the products in your bag \
-                        wasn't found in our database. "
+                        "One of the products in your bag wasn't found in our database. "
                         "Please call us for assistance!")
                     )
                     order.delete()
                     return redirect(reverse('view_bag'))
-            
+
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
-            messsages.error(
-                request,
-                'There was an error with your form \
-                Please double check your information'
-            )
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
     else:
         bag = request.session.get('bag', {})
         if not bag:
@@ -106,15 +78,11 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        # print(intent)
-
-        order_form = OrderForm()  # instance of the order form
+        order_form = OrderForm()
 
     if not stripe_public_key:
-        messages.warning(
-            request,
-            'Stripe public key is missing. \
-                Did you forget to set it in your environment?')
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
 
     template = 'checkout/checkout.html'
     context = {
@@ -134,7 +102,7 @@ def checkout_success(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
-        email will be sent to {order.email}.')
+        email will be sent to {order.email}')
 
     if 'bag' in request.session:
         del request.session['bag']
