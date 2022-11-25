96068aff6274daffdde97f320ac2a95327af338c
Karol Mileszko <karol.miles@gmail.com>
2022-11-25 13:42:56 +0000

    Add Product Admin - Product Form

--- products/forms.py
+++ products/forms.py
@@ -11,5 +11,12 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
+
         # List comprehension - for loop to add items to a list
+        # to create a list of tuples of friendly names
+        # associated with their category ids:
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
