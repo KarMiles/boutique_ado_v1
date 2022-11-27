c227a2c161c5e859a6f1a45c8bec6511c38a75a1
Karol Mileszko <karol.miles@gmail.com>
2022-11-27 12:51:46 +0000

    Product Admin - Deleting Products 2

--- products/views.py
+++ products/views.py
@@ -72,6 +72,12 @@ def product_detail(request, product_id):
     """
 
     product = get_object_or_404(Product, pk=product_id)
+    print('This is product:')
+    print(product)
+    print('This is Product:')
+    print(Product)
+    print('This is product_id:')
+    print(product_id)
 
     context = {
         'product': product,
@@ -87,9 +93,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. \
                 Please ensure the form is valid.')
@@ -129,3 +135,27 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+    product = get_object_or_404(Product, pk=product_id)
+    form = ProductForm(instance=product)
+    messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
