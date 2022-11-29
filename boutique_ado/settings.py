e0a924f7dcab1868c0141400f22d7d4d08d70265
Karol Mileszko <karol.miles@gmail.com>
2022-11-29 13:27:50 +0000

    Add ElephantSQL settings and deploy to Heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 from os import path
@@ -33,7 +34,7 @@ SECRET_KEY = 'django-insecure-xeg%$!%))0ru%*uo@$rnaq(yh4_c57(+$_yzic=v_qlc5romlt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['heroku-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -127,13 +128,18 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            # 'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -52,9 +52,21 @@ class StripeWH_Handler:
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
 
+        # STRIPE UPDATE
+        # Get the Charge object
+        # stripe_charge = stripe.Charge.retrieve(
+        #     intent.latest_charge
+        # )
+
+        # STRIPE UPDATE - OLD VERSION
         billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
         grand_total = round(intent.charges.data[0].amount / 100, 2)
+
+        # STRIPE UPDATE
+        # billing_details = stripe_charge.billing_details  # updated
+        # shipping_details = intent.shipping
+        # grand_total = round(stripe_charge.amount / 100, 2)  # updated
 
         # Clean data in the shipping details
         for field, value in shipping_details.address.items():
